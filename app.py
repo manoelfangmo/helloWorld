d189c8690eeb576070a4946e65e1286c26309d37
Manoel <manoelfangmo@gmail.com>
2023-03-28 15:15:03 -0400

    -Created Favorite Course and Contact HTML Files -Added routes to html files to app.py -created base.html which contains bootstrap navbar -Put navbar on favorite-course.html and base.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,18 @@ def hello():
 @app.route('/about')
 def about():
     return render_template('about.html');
+
+@app.route('/favorite-Course')
+def favoriteCourse():
+    text = str(request.args.get('schoolName')) + " " + str(request.args.get('courseNumber'));
+    return render_template('favorite-course.html',courses=text);
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True);
+    else:
+        return render_template('contact.html');
 
 if __name__ == '__main__':
     app.run()
